c9def34bb0fade8a37371bacf1944c9260f1c5e6
Kamal Deep <86801232+Deepgautam76@users.noreply.github.com>
2025-06-05 23:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function mincost(arr)
 { 
 //write your code here
-// return the min cost
-  
+// return the min cost 
+	let sum=0,prev=0;
+	arr.forEach((item)=>{
+		// prev=sum;
+      sum=sum+item;
+	})
+	return sum;
 }
 
 module.exports=mincost;
