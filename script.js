240133b1d55937992cb11e0fbde5602b6cc3fb37
Kamal Deep <86801232+Deepgautam76@users.noreply.github.com>
2025-06-10 16:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ function mincost(arr)
 // return the min cost 
 	let sum=0;
 	while(arr.length>1){
-		arr.sort((a,b)=>a-b);
-		const cost=arr[0]+arr[1];
-		arr.splice(0,2);
+	arr.sort((a,b)=>a-b);
+		const cost=arr.shift()+arr.shift();
 		sum+=cost;
 		arr.push(sum);
      }
